801af76415abe508eaa4d349b087688a2be03259
sunxi <sunxi_mail@163.com>
2021-06-22 10:26:41 +0800

    chapter four JSON HTML

---
+++ chapter_four/issues/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"go-test/chapter_four/github"
+	"log"
+	"os"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n",
+			item.Number, item.User.Login, item.Title)
+	}
+}
