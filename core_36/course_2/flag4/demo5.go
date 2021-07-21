2c0ea98a25da632ba64896105a5dc171449ca750
sunxi <sunxi_mail@163.com>
2021-07-21 10:01:05 +0800

    core_36 course_2 demo code

---
+++ core_36/course_2/flag4/demo5.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var name string
+var cmdLine = flag.NewFlagSet("question", flag.ExitOnError)
+
+func init() {
+	cmdLine.StringVar(&name, "name", "everyone", "The greeting object.")
+}
+
+func main() {
+	cmdLine.Parse(os.Args[1:])
+	fmt.Printf("Hello, %s!\n", name)
+}
